5354bc28dc3a4c35d3d0bab3c87143741438a8ea
Timothy Qin <timothy.t.qin@gmail.com>
2020-04-25 22:05:45 -0500

    display listing

--- frontend/src/actions/fetchListings.js
+++ frontend/src/actions/fetchListings.js
@@ -1,65 +1,66 @@
-import axios from 'axios';
-import { RECEIVE_LISTINGS } from './types';
-import { showLoading, hideLoading } from 'react-redux-loading-bar';
+import axios from "axios";
+import { RECEIVE_LISTINGS } from "./types";
+import { showLoading, hideLoading } from "react-redux-loading-bar";
 
-const receiveListings = (data) => {
-    return {
-        type: RECEIVE_LISTINGS,
-        payload: data,
-    };
+const receiveListings = data => {
+  return {
+    type: RECEIVE_LISTINGS,
+    payload: data
+  };
 };
-export const fetchListings = () => async (dispatch) => {
-    dispatch(showLoading());
+export const fetchListings = () => async dispatch => {
+  dispatch(showLoading());
 
-    // liquors.get().then(async (snapshot) => {
-    //     var listings = {};
-    //     for (let doc of snapshot.docs) {
-    //         const data = doc.data();
-    //         const url = await storage.refFromURL(data.img).getDownloadURL();
-    //         listings.push({ id: doc.id, ...data, img: url });
-    //     }
-    //     dispatch(receiveListings(listings));
-    //     dispatch(hideLoading());
-    // });
+  // liquors.get().then(async (snapshot) => {
+  //     var listings = {};
+  //     for (let doc of snapshot.docs) {
+  //         const data = doc.data();
+  //         const url = await storage.refFromURL(data.img).getDownloadURL();
+  //         listings.push({ id: doc.id, ...data, img: url });
+  //     }
+  //     dispatch(receiveListings(listings));
+  //     dispatch(hideLoading());
+  // });
 
-    let listings = [
-        {
-            id: 0,
-            type: 'laptop',
-            manufacturer: 'Apple',
-            model: 'Macbook Pro 2019 16 inch',
-            img:
-                'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mbp13touch-space-select-201807?wid=904&hei=840&fmt=jpeg&qlt=80&op_usm=0.5,0.5&.v=1529520060550',
-        },
-        {
-            id: 1,
-            type: 'laptop',
-            manufacturer: 'Dell',
-            model: 'XPS 2019 13 inch',
-            img:
-                'https://images-na.ssl-images-amazon.com/images/I/71jxN6pEeNL._AC_SL1496_.jpg',
-        },
-        {
-            id: 2,
-            type: 'tablet',
-            manufacturer: 'Apple',
-            model: 'iPad Air 2',
-            img:
-                'https://ss7.vzw.com/is/image/VerizonWireless/apple-ipadair2-spacegrey?$device-lg$&wid=256&hei=520',
-        },
-        {
-            id: 3,
-            type: 'textbook',
-            title: "Computer Systems A Programmer's Perspective",
-            course: 'CS429',
-            img:
-                'https://farm4.staticflickr.com/3435/3836184258_3d0418a2c0_b.jpg',
-        },
-    ];
+  let listings = [
+    {
+      id: 0,
+      type: "laptop",
+      manufacturer: "Apple",
+      model: "Macbook Pro 2019 16 inch",
+      img:
+        "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mbp13touch-space-select-201807?wid=904&hei=840&fmt=jpeg&qlt=80&op_usm=0.5,0.5&.v=1529520060550"
+    },
+    {
+      id: 1,
+      type: "laptop",
+      manufacturer: "Dell",
+      model: "XPS 2019 13 inch",
+      img:
+        "https://images-na.ssl-images-amazon.com/images/I/71jxN6pEeNL._AC_SL1496_.jpg"
+    },
+    {
+      id: 2,
+      type: "tablet",
+      manufacturer: "Apple",
+      model: "iPad Air 2",
+      img:
+        "https://ss7.vzw.com/is/image/VerizonWireless/apple-ipadair2-spacegrey?$device-lg$&wid=256&hei=520"
+    },
+    {
+      id: 3,
+      type: "textbook",
+      title: "Computer Systems A Programmer's Perspective",
+      course: "CS429",
+      img: "https://farm4.staticflickr.com/3435/3836184258_3d0418a2c0_b.jpg"
+    }
+  ];
 
-    const token = localStorage.getItem('FBIdToken');
-    axios.defaults.headers.common['Authorization'] = token;
-    axios.get('/listing').then((res) => console.log(res)); // TODO
-    dispatch(receiveListings(listings));
-    dispatch(hideLoading());
+  const token = localStorage.getItem("FBIdToken");
+  axios.defaults.headers.common["Authorization"] = token;
+  axios.get("/listing").then(res => {
+    console.log(JSON.stringify(res.data));
+    dispatch(receiveListings(res.data));
+  }); // TODO
+  dispatch(hideLoading());
 };
--- frontend/src/containers/Listing.jsx
+++ frontend/src/containers/Listing.jsx
@@ -1,118 +1,73 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import { getListing } from '../selectors';
-import product from '../css/product.module.css';
-import { Card, Button, makeStyles } from '@material-ui/core';
+import React from "react";
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { getListing } from "../selectors";
+import product from "../css/product.module.css";
+import { Card, Button, makeStyles } from "@material-ui/core";
 
 const useStyles = makeStyles({
-    root: {
-        background: 'var(--bg-gradient)',
-        padding: '1rem',
-        gridArea: 'img',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    button: {
-        width: '100%',
-        background: 'var(--gold-gradient)',
-        color: 'var(--bg-color)',
-        fontWeight: '700',
-        marginTop: '1rem',
-    },
+  root: {
+    background: "var(--bg-gradient)",
+    padding: "1rem",
+    gridArea: "img",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  button: {
+    width: "100%",
+    background: "var(--gold-gradient)",
+    color: "var(--bg-color)",
+    fontWeight: "700",
+    marginTop: "1rem"
+  }
 });
-const Listing = (props) => {
-    const { id } = useParams();
-    const [listing, setListing] = React.useState(
-        useSelector((state) => getListing(state, id))
-    );
-    console.log(listing);
-    const classes = useStyles();
-    return (
-        <>
-            {listing && (
-                <div className={product.bg}>
-                    <Card className={classes.root}>
-                        <img
-                            className={product.img}
-                            src={listing.img}
-                            alt={`product id: ${id}`}
-                        />
-                    </Card>
-                    <div className={product.title_container}>
-                        <h1 className={product.name}>
-                            {listing.type === 'wine' &&
-                                `${listing.winery} ${listing.varietal}, ${listing.vintage}`}
-                            {listing.type === 'beer' && `${listing.producer}`}
-                        </h1>
-
-                        <span className={product.size}>
-                            {listing.size + 'ml, ' + listing.pack + ' pack'}
-                        </span>
-                    </div>
-                    <div className={product.action_container}>
-                        <span className={product.price}>
-                            {'$' + listing.salePrice}
-                        </span>
-                        <Button className={classes.button}>Add to Cart</Button>
-                    </div>
-                    <div className={product.details_container}>
-                        {listing.category && (
-                            <div className={product.details_row}>
-                                <div className={product.details_label}>
-                                    category
-                                </div>
-                                <div className={product.details_text}>
-                                    {listing.category}
-                                </div>
-                            </div>
-                        )}
-                        {listing.country && (
-                            <div className={product.details_row}>
-                                <div className={product.details_label}>
-                                    country
-                                </div>
-                                <div className={product.details_text}>
-                                    {listing.country}
-                                </div>
-                            </div>
-                        )}
-                        {listing.type && (
-                            <div className={product.details_row}>
-                                <div className={product.details_label}>
-                                    type
-                                </div>
-                                <div className={product.details_text}>
-                                    {listing.type}
-                                </div>
-                            </div>
-                        )}
-                        {listing.varietal && (
-                            <div className={product.details_row}>
-                                <div className={product.details_label}>
-                                    varietal
-                                </div>
-                                <div className={product.details_text}>
-                                    {listing.varietal}
-                                </div>
-                            </div>
-                        )}
-                        {listing.winery && (
-                            <div className={product.details_row}>
-                                <div className={product.details_label}>
-                                    winery
-                                </div>
-                                <div className={product.details_text}>
-                                    {listing.winery}
-                                </div>
-                            </div>
-                        )}
-                    </div>
-                </div>
+const Listing = props => {
+  const { id } = useParams();
+  const [listing, setListing] = React.useState(
+    useSelector(state => getListing(state, id))
+  );
+  console.log(listing);
+  const classes = useStyles();
+  return (
+    <>
+      {listing && (
+        <div className={product.bg}>
+          <Card className={classes.root}>
+            <img
+              className={product.img}
+              src={listing.listingImage}
+              alt={`product id: ${id}`}
+            />
+          </Card>
+          <div className={product.title_container}></div>
+          <div className={product.action_container}>
+            <span className={product.price}>{listing.title}</span>
+            <Button className={classes.button}>Request</Button>
+          </div>
+          <div className={product.details_container}>
+            {listing.type && (
+              <div className={product.details_row}>
+                <div className={product.details_label}>type</div>
+                <div className={product.details_text}>{listing.type}</div>
+              </div>
             )}
-        </>
-    );
+            {listing.body &&
+              Object.keys(listing.body).map(key => {
+                return (
+                  <div className={product.details_row}>
+                    <div className={product.details_label}>{key}</div>
+                    <div className={product.details_text}>
+                      {listing.body[key]}
+                    </div>
+                  </div>
+                );
+              })}
+          </div>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default Listing;
