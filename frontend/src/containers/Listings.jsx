5cb155f51838362737d201a6dd0661e78417126e
Timothy Qin <timothy.t.qin@gmail.com>
2020-04-26 07:19:22 -0500

    added add listing button/link

--- frontend/src/containers/Listings.jsx
+++ frontend/src/containers/Listings.jsx
@@ -1,15 +1,28 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import AddIcon from "@material-ui/icons/Add";
 import Filter from "../components/Filter";
 import styles from "../css/listings.module.css";
 import ListingCard from "../components/ListingCard";
 import { getFilteredListings } from "../selectors";
+import { Button } from "@material-ui/core";
 
 const Listings = props => {
   const listings = useSelector(state => getFilteredListings(state));
   return (
     <div className={styles.bg}>
-      <Filter />
+      <div>
+        <Button
+          variant="contained"
+          color="secondary"
+          style={{ margin: "1em 2.5em" }}
+          href="/listings/create"
+        >
+          Add Listing
+          <AddIcon />
+        </Button>
+        <Filter />
+      </div>
       <main className={styles.grid}>
         {listings.map(item => {
           console.log(item);
