88b1ef9dd131a17319ede157f407f759e7ecd00d
Steve Han <stevehan2001@gmail.com>
2020-04-26 05:41:47 -0500

    started on chatlist

--- frontend/src/containers/ChatList.js
+++ frontend/src/containers/ChatList.js
@@ -1,17 +1,36 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React, { useState, useEffect } from 'react';
+import { chatroomsdb } from '../firebase';
+import axios from 'axios';
+import { useSelector } from 'react-redux';
 
 export default function ChatList() {
-  const [loading, setLoading] = useState(true);
-  const [userCredentials, setUserCredentials] = useState({});
-  return (
-    <div>
-      {!loading &&
-        userCredentials.chatrooms.map(room =>
-          room.members[0] === userCredentials.credentials.username
-            ? room.members[1]
-            : room.members[0]
-        )}
-    </div>
-  );
+    const username = useSelector((state) => state.user.credentials.username);
+    const [chatrooms, setChatrooms] = useState([]);
+    useEffect(() => {
+        if (username) {
+            let getMyChatrooms = chatroomsdb.where(
+                'members',
+                'array-contains',
+                username
+            );
+            getMyChatrooms.onSnapshot((querySnapshot) => {
+                querySnapshot.forEach((chatroom) => {
+                    setChatrooms((chatrooms) => {
+                        // prevent duplicates
+                        for (let cr of chatrooms) {
+                            if (cr.id === chatroom.id) {
+                                return chatrooms;
+                            }
+                        }
+                        return [
+                            ...chatrooms,
+                            { ...chatroom.data(), id: chatroom.id },
+                        ];
+                    });
+                });
+            });
+        }
+    }, [username]);
+
+    return <div></div>;
 }
--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -12,4 +12,5 @@ const firebaseConfig = {
 };
 firebase.initializeApp(firebaseConfig);
 const storage = firebase.storage();
-export { storage };
+const chatroomsdb = firebase.firestore().collection('chatrooms');
+export { storage, chatroomsdb };
