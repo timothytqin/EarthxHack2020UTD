89149060df619e51d87c1de24268d7a029b971b0
Steve Han <stevehan2001@gmail.com>
2020-04-25 22:52:03 -0500

    changed listing

--- frontend/src/components/ListingCard.jsx
+++ frontend/src/components/ListingCard.jsx
@@ -1,40 +1,40 @@
-import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import {
-  withStyles,
-  Button,
-  Card,
-  CardActionArea,
-  CardMedia,
-  CardContent,
-  Typography,
-  CardActions
-} from "@material-ui/core";
-import { Link } from "react-router-dom";
+    withStyles,
+    Button,
+    Card,
+    CardActionArea,
+    CardMedia,
+    CardContent,
+    Typography,
+    CardActions,
+} from '@material-ui/core';
+import { Link } from 'react-router-dom';
 
 const ListingCard = ({ listing }) => {
-  return (
-    <Card>
-      <Link
-        to={`/app/${listing.listingId}`}
-        style={{ color: "inherit", textDecoration: "inherit" }}
-      >
-        <CardActionArea>
-          <CardMedia image={listing.listingImage} />
-          <CardContent>
-            <Typography variant="h5" color="primary">
-              {listing.title}
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-      </Link>
-      <CardActions>
-        <Button variant="contained" color="secondary">
-          Request
-        </Button>
-      </CardActions>
-    </Card>
-  );
+    return (
+        <Card>
+            <Link
+                to={`/listings/${listing.listingId}`}
+                style={{ color: 'inherit', textDecoration: 'inherit' }}
+            >
+                <CardActionArea>
+                    <CardMedia image={listing.listingImage} />
+                    <CardContent>
+                        <Typography variant="h5" color="primary">
+                            {listing.title}
+                        </Typography>
+                    </CardContent>
+                </CardActionArea>
+            </Link>
+            <CardActions>
+                <Button variant="contained" color="secondary">
+                    Request
+                </Button>
+            </CardActions>
+        </Card>
+    );
 };
 
 export default ListingCard;
