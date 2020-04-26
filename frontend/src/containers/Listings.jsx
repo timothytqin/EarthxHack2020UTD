4cf425f8429576ee54460e03b84d79984641bfdd
Steve Han <stevehan2001@gmail.com>
2020-04-25 23:21:02 -0500

    fixed errors

--- frontend/src/containers/Listings.jsx
+++ frontend/src/containers/Listings.jsx
@@ -1,24 +1,26 @@
-import React from "react";
-import { useSelector } from "react-redux";
-import Filter from "../components/Filter";
-import styles from "../css/listings.module.css";
-import ListingCard from "../components/ListingCard";
-import { getFilteredListings } from "../selectors";
+import React from 'react';
+import { useSelector } from 'react-redux';
+import Filter from '../components/Filter';
+import styles from '../css/listings.module.css';
+import ListingCard from '../components/ListingCard';
+import { getFilteredListings } from '../selectors';
 
-const Products = props => {
-  const listings = useSelector(state => getFilteredListings(state));
-  console.log("LISTINGCARD: " + JSON.stringify(listings));
-  return (
-    <div className={styles.bg}>
-      <Filter />
-      <main className={styles.grid}>
-        {listings.map(item => {
-          console.log(item);
-          return <ListingCard listing={item} key={item.listingId} />;
-        })}
-      </main>
-    </div>
-  );
+const Products = (props) => {
+    const listings = useSelector((state) => getFilteredListings(state));
+    console.log('LISTINGCARD: ' + JSON.stringify(listings));
+    return (
+        <div className={styles.bg}>
+            <Filter />
+            <main className={styles.grid}>
+                {listings.map((item) => {
+                    console.log(item);
+                    return (
+                        <ListingCard listing={item} key={item.body.listingId} />
+                    );
+                })}
+            </main>
+        </div>
+    );
 };
 
 export default Products;
